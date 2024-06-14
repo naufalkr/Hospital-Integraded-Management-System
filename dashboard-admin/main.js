2dd78c19b2c1f8911b035276de51d01e930a3622
wafizaki <wafizakihanif@gmail.com>
2024-06-14 13:15:55 +0700

    update dashboard-admin

--- dashboard-admin/main.js
+++ dashboard-admin/main.js
@@ -1,34 +1,3 @@
-// const content = document.getElementById("content");
-// const addContentButton = document.getElementById("tambahButton");
-
-// function getNotes() {
-//   return JSON.parse(localStorage.getItem("list-notes")) || [];
-// }
-
-// function saveNotes(notes) {
-//   localStorage.setItem("list-notes", JSON.stringify(notes));
-// }
-
-// function createNoteElement(id, content) {
-//   const element = document.createElement("textarea");
-
-//   element.classList.add("note");
-//   element.classList.add("bg-gray-100");
-//   element.classList.add("border");
-//   element.classList.add("border-gray-300");
-//   element.classList.add("rounded-md");
-//   element.classList.add("p-2");
-//   element.classList.add("resize-none");
-//   element.value = content;
-//   element.placeholder = "Masukkan catatan disini";
-
-//   element.addEventListener("input", (e) => {});
-
-//   return element;
-// }
-
-// function addNote() {}
-
 const tenagaMedisView = `
       <div class="flex flex-row justify-between">
       <div class="flex-col">
@@ -44,22 +13,29 @@ const tenagaMedisView = `
       `;
 
 const tenagaMedis = `
- <h1 class="text-teal-500">Search</h1>
-  <div class="h-[60vh] overflow-y-scroll rounded-lg">
-    <ul>
+<div class="flex flex-row justify-between">
+<h1 class="text-black">Daftar Tenaga Medis</h1>
+ <button
+            class="font-bold text-black rounded p-2 hover:bg-red-500 hover:text-white right-6"
+          >
+            Keluar
+          </button>
+</div>
+  <div class="h-[80vh] overflow-y-scroll rounded-lg">
+   <ul>
     ${Array.from(
       { length: 15 },
       (_, i) => `
-      <li class="flex items-center justify-between py-2 shadow-md m-4">
-      <div class="flex items-center">
-        <img src="profile_picture_${
-          i + 1
-        }.jpg" class="w-10 h-10 rounded-full mr-2">
-        <span>Dokter ${i + 1}</span>
+      <li class="flex items-center justify-between shadow-md m-4 rounded-lg">
+      <div class="flex flex-row">
+      <img src="avatar.jpg" class="w-20 h-20 rounded-full mx-4 my-2">
+        <div class="flex items-center justify-center flex-col">
+        <p class="text-sm">Nama Dokter ${i + 1}</p>
+        <p class="text-xs text-gray-400">Opname</p>
+        </div>
       </div>
       <div>
-        <button class="hover:bg-teal-500 hover:rounded-lg p-2 py-1" onclick="switchContent('Tenaga Medis View')">View</button>
-        <button class="hover:bg-teal-500 hover:rounded-lg p-2 py-1">Delete</button>
+        <button class="hover:bg-teal-500 hover:text-white hover:rounded-lg p-2 py-1 my-2 mx-4" onclick="switchContent('Tenaga Medis View')">View</button>
       </div>
       </li>
     `
@@ -84,22 +60,29 @@ const pasienView = `
       `;
 
 const pasien = `
- <h1 class="text-teal-500">Search</h1>
-  <div class="h-[60vh] overflow-y-scroll rounded-lg">
+ <div class="flex flex-row justify-between">
+<h1 class="text-black">Daftar Tenaga Medis</h1>
+ <button
+            class="font-bold text-black rounded p-2 hover:bg-red-500 hover:text-white right-6"
+          >
+            Keluar
+          </button>
+</div>
+  <div class="h-[80vh] overflow-y-scroll rounded-lg">
     <ul>
     ${Array.from(
       { length: 15 },
       (_, i) => `
-      <li class="flex items-center justify-between py-2 shadow-md m-4">
-      <div class="flex items-center">
-        <img src="profile_picture_${
-          i + 1
-        }.jpg" class="w-10 h-10 rounded-full mr-2">
-        <span>Pasien ${i + 1}</span>
+      <li class="flex items-center justify-between shadow-md m-4 rounded-lg">
+      <div class="flex flex-row">
+      <img src="avatar.jpg" class="w-20 h-20 rounded-full mx-4 my-2">
+        <div class="flex items-center justify-center flex-col">
+        <p class="text-sm">NIK Pasien ${i + 1}</p>
+        <p class="text-xs text-gray-400">Opname</p>
+        </div>
       </div>
       <div>
-        <button class="hover:bg-teal-500 hover:rounded-lg p-2 py-1" onclick="switchContent('Pasien View')">View</button>
-        <button class="hover:bg-teal-500 hover:rounded-lg p-2 py-1">Delete</button>
+        <button class="hover:bg-teal-500 hover:text-white hover:rounded-lg p-2 py-1 my-2 mx-4" onclick="switchContent('Pasien View')">View</button>
       </div>
       </li>
     `
@@ -107,82 +90,6 @@ const pasien = `
     </ul>
   </div>
   `;
-
-const rumahSakitView = `
-         <div class="flex flex-row justify-between">
-      <div class="flex-col">
-        <h3 class="text-xl mb-4">Rumah Sakit</h3>
-          <h3 class="text-xl mb-4">Nama: Rumah Sakit Keputih</h3>
-          <h3 class="text-xl mb-4">Alamat: Keputih Tegal Timur</h3>
-          <h3 class="text-xl mb-4">No Telpon: 08999999999</h3>
-          <h3 class="text-xl mb-4">Kota: Bangkok </h3>
-          </div>
-      <h1>foto??</h1>
-      </div>
-      `;
-
-const rumahSakit = `
-  <h1 class="text-teal-500">Search</h1>
-  <div class="h-[60vh] overflow-y-scroll rounded-lg">
-    <ul>
-    ${Array.from(
-      { length: 15 },
-      (_, i) => `
-      <li class="flex items-center justify-between py-2 shadow-md m-4">
-      <div class="flex items-center">
-        <img src="profile_picture_${
-          i + 1
-        }.jpg" class="w-10 h-10 rounded-full mr-2">
-        <span>Rumah Sakit ${i + 1}</span>
-      </div>
-      <div>
-        <button class="hover:bg-teal-500 hover:rounded-lg p-2 py-1" onclick="switchContent('Pasien View')">View</button>
-        <button class="hover:bg-teal-500 hover:rounded-lg p-2 py-1">Delete</button>
-      </div>
-      </li>
-    `
-    ).join("")}
-    </ul>
-  </div>
-  `;
-
-const obatView = `
-        <div class="flex flex-row justify-between">
-      <div class="flex-col">
-        <h3 class="text-xl mb-4">Obat</h3>
-          <h3 class="text-xl mb-4">Nama: Komix</h3>
-          <h3 class="text-xl mb-4">Deskripsi: Dibuat dari kulit ayam serta ekstrak lidah buaya</h3>
-          </div>
-      <h1>foto</h1>
-      </div>
-      `;
-
-const obat = `
-  <h1 class="text-teal-500">Search</h1>
-  <div class="h-[60vh] overflow-y-scroll rounded-lg">
-    <ul>
-      ${Array.from(
-        { length: 15 },
-        (_, i) => `
-        <li class="flex items-center justify-between py-2 shadow-md m-4">
-          <div class="flex items-center">
-            <img src="profile_picture_${
-              i + 1
-            }.jpg" class="w-10 h-10 rounded-full mr-2">
-            <span>Obat ${i + 1}</span>
-          </div>
-          <div>
-            <button class="hover:bg-teal-500 hover:rounded-lg p-2 py-1" onclick="switchContent('Obat View')">View</button>
-            <button class="hover:bg-teal-500 hover:rounded-lg p-2 py-1" onclick="deleteObat(${
-              i + 1
-            })">Delete</button>
-          </div>
-        </li>
-      `
-      ).join("")}
-    </ul>
-  </div>
-`;
 
 function switchContent(contentType) {
   switch (contentType) {
@@ -197,18 +104,6 @@ function switchContent(contentType) {
       break;
     case "Pasien View":
       content.innerHTML = pasienView;
-      break;
-    case "Rumah Sakit":
-      content.innerHTML = rumahSakit;
-      break;
-    case "Rumah Sakit View":
-      content.innerHTML = rumahSakitView;
-      break;
-    case "Obat":
-      content.innerHTML = obat;
-      break;
-    case "Obat View":
-      content.innerHTML = obatView;
       break;
     default:
       content.innerHTML = "";
